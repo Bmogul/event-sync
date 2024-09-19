2789d43fe863691b04df3365998a001211b0a525
b.mogul02@gmail.com <b.mogul02@gmail.com>
2024-09-19 13:59:32 -0400

    created loading circle

--- src/app/events/components/loading.js
+++ src/app/events/components/loading.js
@@ -1,7 +1,20 @@
-const loading = () => {
-  return (
-    <>Loading...</>
-  )
-}
+import React from 'react';
+import styles from '../styles/loading.module.css';
 
-export default loading
+const LoadingCircle = ({ size = 50, borderWidth = 4, color = '#4CAF50' }) => {
+  return (
+    <div className={styles.loadingContainer}>
+      <div
+        className={styles.loadingCircle}
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+          borderWidth: `${borderWidth}px`,
+          borderTopColor: color
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default LoadingCircle;

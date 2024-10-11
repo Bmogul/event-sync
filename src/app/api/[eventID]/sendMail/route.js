fd7344b8af0b36a8b2f5876ab7fb54fc70cbf76b
b.mogul02@gmail.com <b.mogul02@gmail.com>
2024-10-11 18:04:15 -0400

    persist sent

--- src/app/api/[eventID]/sendMail/route.js
+++ src/app/api/[eventID]/sendMail/route.js
@@ -39,12 +39,10 @@ export const POST = async (req) => {
             const msg = {
               to: guest.Email,
               from: sender,
-              subject: reminder
+              subject: !reminder
                 ? "Invitation inside"
                 : "Important reminder inside",
-              html: reminder
-                ? compiledReminderTemplate(templateData)
-                : compiledInviteTemplate(templateData),
+              html:compiledInviteTemplate(templateData)
             };
             await sgMail.send(msg);
             console.log(
@@ -59,6 +57,59 @@ export const POST = async (req) => {
         }),
       );
 
+      // Update the sheet
+      // Get existing data from sheet
+      const mainSheet = await sheets.spreadsheets.values.get({
+        spreadsheetId: sheetID,
+        range: "Main",
+      });
+      const allValues = mainSheet.data.values || [];
+      const valuesToUpdate = {};
+
+      // loop through party and get values to update
+      for (const member of updatedGuestList) {
+        const { GUID, UID } = member;
+
+        if (!GUID || !UID) {
+          continue; // Skip members without GUID or UID
+        }
+        console.log(member.Sent);
+
+        allValues.filter((row) => {
+          console.log(row[0]);
+          if (row[0].toString() === GUID && row[1].toString() === UID) {
+            const rowNumber = allValues.indexOf(row) + 1;
+            const cellRange = `Main!J${rowNumber}:K${rowNumber}`;
+            const values = [member.Sent];
+
+            valuesToUpdate[cellRange] = {
+              values: [values],
+            };
+            return true;
+          }
+          return false;
+        });
+      }
+
+      // Batch Update values in sheet
+      if (Object.keys(valuesToUpdate).length > 0) {
+        const batchUpdateRequest = {
+          data: [],
+          valueInputOption: "RAW",
+        };
+
+        for (const [range, valueData] of Object.entries(valuesToUpdate)) {
+          batchUpdateRequest.data.push({
+            range,
+            ...valueData,
+          });
+        }
+
+        sheets.spreadsheets.values.batchUpdate({
+          spreadsheetId: process.env.GOOGLE_SHEET_ID,
+          resource: batchUpdateRequest,
+        });
+      }
       console.log(updatedGuestList);
       return NextResponse.json(
         { validated: true, guestList: updatedGuestList },

e0d33658076cc442f6a64a42568bd4e9c5578684
b.mogul02@gmail.com <b.mogul02@gmail.com>
2024-10-13 13:21:59 -0400

    bug with mobile

--- src/app/[eventID]/components/RSVPForm.js
+++ src/app/[eventID]/components/RSVPForm.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import styles from '../styles/form.module.css'
+import React, { useState } from "react";
+import styles from "../styles/form.module.css";
 import Image from "next/image";
-import Loading from './loading'
+import Loading from "./loading";
 
 const ResBlock = ({ member, onResponseChange }) => {
-  const [response, setResponse] = useState(member.MainResponse)
+  const [response, setResponse] = useState(member.MainResponse);
 
   const handleResponse = (value) => {
-    const newResponse = response === value ? "" : value
-    setResponse(newResponse)
-    onResponseChange(member.UID, newResponse)
-  }
+    const newResponse = response === value ? "" : value;
+    setResponse(newResponse);
+    onResponseChange(member.UID, newResponse);
+  };
 
   return (
     <div className={styles.resBlock}>
@@ -19,33 +19,41 @@ const ResBlock = ({ member, onResponseChange }) => {
       </div>
       <div>
         <button
-          className={`${styles.responseBtn} ${response === "1" ? styles.greenBtn : ''}`}
+          className={`${styles.responseBtn} ${response === "1" ? styles.greenBtn : ""}`}
           onClick={() => handleResponse("1")}
         >
           Yes
         </button>
         <button
-          className={`${styles.responseBtn} ${response === "0" ? styles.greenBtn : ''}`}
+          className={`${styles.responseBtn} ${response === "0" ? styles.greenBtn : ""}`}
           onClick={() => handleResponse("0")}
         >
           No
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const RsvpForm = ({ formLoading, closeForm, party, setParty, postResponse, event }) => {
-
-  const [sortedParty, setSortedParty] = useState([...party].sort((a, b) => a.FamilyOrder - b.FamilyOrder));
+const RsvpForm = ({
+  formLoading,
+  closeForm,
+  party,
+  setParty,
+  postResponse,
+  event,
+}) => {
+  const [sortedParty, setSortedParty] = useState(
+    [...party].sort((a, b) => a.FamilyOrder - b.FamilyOrder),
+  );
 
   const onResChange = (key, res) => {
-    setParty(prevParty =>
-      prevParty.map(member =>
-        member.UID === key ? { ...member, MainResponse: res } : member
-      )
-    )
-  }
+    setParty((prevParty) =>
+      prevParty.map((member) =>
+        member.UID === key ? { ...member, MainResponse: res } : member,
+      ),
+    );
+  };
 
   return (
     <dialog open className={styles.modal}>
@@ -54,33 +62,68 @@ const RsvpForm = ({ formLoading, closeForm, party, setParty, postResponse, event
       ) : (
         <>
           <div className={styles.closeBtnDiv}>
-            <Image onClick={closeForm} src={"/close_ring.svg"} alt="Close Form" height={50} width={50} className={styles.closeBtn} />
+            <Image
+              onClick={closeForm}
+              src={"/close_ring.svg"}
+              alt="Close Form"
+              height={50}
+              width={50}
+              className={styles.closeBtn}
+            />
           </div>
           <div className={styles.formHeader}>
             <h3>{event.func0.funcTitle}</h3>
             <div className={styles.headerInfo}>
-              <Image onClick={closeForm} src={"/Date_range_fill.svg"} alt="Close Form" height={25} width={25} className={styles.closeBtn} />
+              <Image
+                onClick={closeForm}
+                src={"/Date_range_fill.svg"}
+                alt="Close Form"
+                height={25}
+                width={25}
+                className={styles.closeBtn}
+              />
               <label>{event.func0.date}</label>
             </div>
             <div className={styles.headerInfo}>
-              <Image onClick={closeForm} src={"/Pin_alt.svg"} alt="Close Form" height={25} width={25} className={styles.closeBtn} />
+              <Image
+                onClick={closeForm}
+                src={"/Pin_alt.svg"}
+                alt="Close Form"
+                height={25}
+                width={25}
+                className={styles.closeBtn}
+              />
               <label>{event.func0.location}</label>
             </div>
           </div>
           <div className={styles.formBody}>
-            {sortedParty.map(member => (
-              <ResBlock key={member.UID} member={member} onResponseChange={onResChange} />
+            {sortedParty.map((member) => (
+              <ResBlock
+                key={member.UID}
+                member={member}
+                onResponseChange={onResChange}
+              />
             ))}
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', height: '40px', fontSize: '20px', marginTop: '30px' }}>
-              <button onClick={postResponse} className={styles.responseBtn}>Submit</button>
-            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                height: "40px",
+                fontSize: "20px",
+                marginTop: "30px",
+              }}
+            ></div>
           </div>
           <div className={styles.formSubmit}>
+            <button onClick={postResponse} className={styles.responseBtn}>
+              Submit
+            </button>
           </div>
         </>
       )}
     </dialog>
-  )
-}
+  );
+};
 
-export default RsvpForm
+export default RsvpForm;
